f050ad679888ed401866f5258c86a486d946a6d2
Sharath Narayan <phsharathnarayan@gmail.com>
2017-08-20 16:33:34 -0500

    Enemy Pests

---
+++ src/com/smoke/Villain/NamekPests.java
@@ -0,0 +1,35 @@
+package com.smoke.Villain;
+
+import com.smoke.Render.*;
+import com.smoke.WorldMap.*;
+
+public class NamekPests extends Render {
+	
+	protected int health;
+	protected int maxHealth;
+	protected boolean dead;
+	protected int damage;
+	
+	protected boolean flinching;
+	protected long flinchTimer;
+	
+	public NamekPests(TileMap tm) {
+		super(tm);
+	}
+	
+	public boolean isDead() { return dead; }
+	
+	public int getDamage() { return damage; }
+	
+	public void hit(int damage) {
+		if(dead || flinching) return;
+		health -= damage;
+		if(health < 0) health = 0;
+		if(health == 0) dead = true;
+		flinching = true;
+		flinchTimer = System.nanoTime();
+	}
+	
+	public void update() {}
+	
+}
